f3caea134846c688d504cc02393ffb645716acf7
Julian Cheal <julian.cheal@gmail.com>
2014-10-17 12:23:36 -0400

    Added the ffi-beacon code.
    This is the code that actually talks to the ibeacon binary. Woot, this gem is comign together!

--- lib/hermes/beacon.rb
+++ lib/hermes/beacon.rb
@@ -2,6 +2,23 @@ require "hermes/beacon/version"
 
 module Hermes
   module Beacon
-    # Your code goes here...
+    extend FFI::Library
+
+    ffi_lib 'objc', '/System/Library/Frameworks/Foundation.framework/Foundation', '/System/Library/Frameworks/IOBluetooth.framework/IOBluetooth',
+    'bin/ibeacon'
+    
+    attach_function :print_version, [], :void
+    callback :completion_function, [:string, :int, :int, :int, :int], :int
+
+    attach_function :startWithTimeInterval, [:double, :completion_function], :void
+
+    def self.scan(interval=1.1)
+      scan = {}
+      callback = Proc.new do |uuid, major, minor, power, rssi|
+        scan = {uuid: uuid, major: major, minor: minor, power: power, rssi: rssi}
+        return scan
+      end
+      Hermes::Beacon.startWithTimeInterval(interval,callback)
+    end
   end
 end
