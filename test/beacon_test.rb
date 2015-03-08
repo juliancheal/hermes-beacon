27bcc36099938305ec16fdc9cb2c5ba85b5521a1
Julian Cheal <julian.cheal@gmail.com>
2015-03-08 17:28:04 +0000

    Missing `end` in test

--- test/beacon_test.rb
+++ test/beacon_test.rb
@@ -6,30 +6,31 @@ describe Hermes::Beacon do
     it 'should return the correct gem version number' do
       Hermes::Beacon::VERSION.must_equal "0.0.2"
     end
+  end
   
   describe 'Distance' do
     it "should return a distance of 1 when rssi & power are equal" do
       scan = Hermes::Beacon::ScanItem.new("uuid", "major", "minor", -59, -59)
       scan.calculate_distance_from_beacon.must_equal 1.01076
     end
-  
+
     it "should return a distance of 2 when rssi is double power" do
       scan = Hermes::Beacon::ScanItem.new("uuid", "major", "minor", -59, -118)
       scan.calculate_distance_from_beacon.must_equal 2
     end
   end
-  
+
   describe 'Ranging' do
     it "should return a Range of Immediate" do
       scan = Hermes::Beacon::ScanItem.new("uuid", "major", "minor", -59, -40)
       scan.range.must_equal "Immediate"
     end
-  
+
     it "should return a Range of Near" do
       scan = Hermes::Beacon::ScanItem.new("uuid", "major", "minor", -59, -69)
       scan.range.must_equal "Near"
     end
-    
+
     it "should return a Range of Far" do
       scan = Hermes::Beacon::ScanItem.new("uuid", "major", "minor", -59, -150)
       scan.range.must_equal "Far"
