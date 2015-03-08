2353d8059f192988e92e038622082ce55b85aa3c
Julian Cheal <julian.cheal@gmail.com>
2015-03-08 23:15:52 +0000

    Fixed distance tests

--- test/beacon_test.rb
+++ test/beacon_test.rb
@@ -8,17 +8,17 @@ describe Hermes::Beacon do
     end
   end
 
-  # describe 'Distance' do
-  #   it "should return a distance of 1 when rssi & power are equal" do
-  #     scan = Hermes::Beacon::ScanItem.new("uuid", "major", "minor", -59, -59)
-  #     scan.calculate_distance_from_beacon.must_equal 1.01076
-  #   end
-  #
-  #   it "should return a distance of 2 when rssi is double power" do
-  #     scan = Hermes::Beacon::ScanItem.new("uuid", "major", "minor", -59, -59)
-  #     scan.calculate_distance_from_beacon.must_equal 2
-  #   end
-  # end
+  describe 'Distance' do
+    it "should return a distance of 1 when rssi & power are equal" do
+      scan = Hermes::Beacon::ScanItem.new("uuid", "major", "minor", -59, -59)
+      scan.calculate_distance_from_beacon.floor.must_equal 1
+    end
+
+    it "should return a distance of 2 when rssi is double power" do
+      scan = Hermes::Beacon::ScanItem.new("uuid", "major", "minor", -59, -65)
+      scan.calculate_distance_from_beacon.floor.must_equal 2
+    end
+  end
 
   describe 'Ranging' do
     it "should return a Range of Immediate" do
