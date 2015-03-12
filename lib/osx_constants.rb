d93ab67f5721290caa9b74c4d7fbf7872b5143fb
Julian Cheal <julian.cheal@gmail.com>
2015-03-12 16:54:24 +0000

    Formatting changes

--- lib/osx_constants.rb
+++ lib/osx_constants.rb
@@ -4,10 +4,10 @@ require 'ffi'
 module Hermes
   module Beacon
     extend FFI::Library
-    
+
     ffi_lib 'objc', '/System/Library/Frameworks/Foundation.framework/Foundation', '/System/Library/Frameworks/IOBluetooth.framework/IOBluetooth',
     "#{File.dirname(__FILE__)}/../bin/hermes-beacon"
-    
+
     def self.scan(interval=0.1)
       scan = {}
       callback = Proc.new do |uuid, major, minor, power, rssi|
@@ -16,9 +16,9 @@ module Hermes
       end
       Beacon.startWithTimeInterval(interval,callback)
     end
-    
+
     callback :completion_function, [:string, :int, :int, :int, :int], :int
-    
+
     attach_function :startWithTimeInterval, [:double, :completion_function], :void
   end
 end
